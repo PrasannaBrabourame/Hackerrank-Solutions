43da04cb13526b0a603d100d006957acafb6be6a
Prasanna Brabourame <IS5442@integra.co.in>
2018-11-07 17:28:10 +0530

    2nd commit

---
+++ 10-Days-of-JavaScript/03-TryCatchFinally.js
@@ -0,0 +1,19 @@
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try {
+        var splitString = s.split("");
+        var reverseArray = splitString.reverse();
+        var joinArray = reverseArray.join("");
+        console.log(joinArray);
+    }
+    catch (error)
+        {
+            console.log(error.message);
+            console.log(s);
+        }
+    
+    
+}
