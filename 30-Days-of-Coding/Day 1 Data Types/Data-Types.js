45cffb78a603054fbc5ce713a99754216cff9e56
Prasanna Brabourame <IS5442@integra.co.in>
2018-11-08 12:48:34 +0530

    Day 2/30

---
+++ 30-Days-of-Coding/Day 1 Data Types/Data-Types.js
@@ -0,0 +1,15 @@
+
+    // Declare second integer, double, and String variables.
+    var secondInteger = 12
+    // Read and save an integer, double, and String to your variables.
+    var sumValue = (Number(input_stdin_array[0]) + i)
+    // Print the sum of both integer variables on a new line.
+    console.log(sumValue)
+
+    // Print the sum of the double variables on a new line.
+    var print = parseFloat(input_stdin_array[1]) + parseFloat('4.0');
+    console.log(print.toFixed(1))
+    // Concatenate and print the String variables on a new line
+    var default1 = "is the best place to learn and practice coding!"
+    // The 's' variable above should be printed first.
+    console.log(s + input_stdin_array[2]);
